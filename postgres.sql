708c1d92c840feb311ec0961daafd1edcfedc808
Jorge Alvarez <44485505+jorgehaq@users.noreply.github.com>
2025-08-01 18:22:06 -0500

    chore(db): add postgres initialization script

---
+++ postgres.sql
@@ -0,0 +1,13 @@
+SELECT * FROM information_schema.tables 
+WHERE table_schema = 'public';
+
+
+
+select * from public.users
+
+
+SELECT * from public.datasets
+
+
+
+update public.datasets set user_id=1
